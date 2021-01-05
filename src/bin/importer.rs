2a117349bbe14542a3c838f389962cc0e44c0d4e
Michael Kreuzer <m-git@kreuzers.org>
2021-01-05 01:53:48 +0100

    moved importer to standalone binary

--- src/bin/importer.rs
+++ src/bin/importer.rs
@@ -1,13 +1,8 @@
 use std::{fs, process::Command};
 
-use chrono::prelude::*;
-use serde::Serialize;
 use walkdir::WalkDir;
 
-use storage::FlightStorage;
-
-mod datafeed;
-mod storage;
+use vatsim_stats::{datafeed, storage::FlightStorage};
 
 fn main() {
     let mut flight_storage = FlightStorage::new();
@@ -37,40 +32,9 @@ fn main() {
         }
     }
 
-    /*
-    let mut departures: Vec<_> = flights
-        .iter()
-        .filter(|(_cs, f)| (f.departure == "EDDM" && f.departure_time.is_some()))
-        .map(|(cs, f)| (cs, f.departure_time.unwrap()))
-        .collect();
-    departures.sort_by_key(|(_cs, time)| *time);
-    for line in &departures {
-        println! {"{:?}", line}
-    }
-    dbg!(departures.len());
-    let mut departures: Vec<_> = flights
-        .iter()
-        .filter(|(_cs, f)| (f.arrival == "EDDM" && f.arrival_time.is_some()))
-        .map(|(cs, f)| (cs, f.arrival_time.unwrap()))
-        .collect();
-    departures.sort_by_key(|(_cs, time)| *time);
-    for line in &departures {
-        println! {"{:?}", line}
-    }
-    dbg!(departures.len());*/
-
     fs::write(
         "storage/flights.ron",
         ron::ser::to_string(&flight_storage).unwrap(),
     )
     .unwrap();
-}
-
-#[derive(Debug, Serialize)]
-struct Flight {
-    //callsign: String,
-    departure: String,
-    arrival: String,
-    departure_time: Option<DateTime<Utc>>,
-    arrival_time: Option<DateTime<Utc>>,
 }
