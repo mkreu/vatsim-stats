df6fd4ae9329ead806001bb94902a64cae3b219d
Michael Kreuzer <m-git@kreuzers.org>
2021-01-23 15:49:15 +0100

    update to tokio ^1 and moved downloader out of server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,20 +1,38 @@
-use std::iter;
+use std::{fs, sync::Arc, time::Duration};
 
-use vatsim_stats::server;
+use log::{info, warn};
+use tokio::sync::RwLock;
+use tokio::time;
+use vatsim_stats::{datafeed, server, storage::FlightStorage};
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    server::run(DummyDataProvider).await.unwrap();
+    let flight_storage: FlightStorage =
+        ron::from_str(&fs::read_to_string("storage/flights.ron").unwrap()).unwrap();
+    let flight_storage = Arc::new(RwLock::new(flight_storage));
+    info!("flight data loaded");
+    tokio::spawn(downloader(flight_storage.clone()));
+    server::run(flight_storage).await.unwrap();
 }
-
-struct DummyDataProvider;
-
-impl server::DataProvider for DummyDataProvider {
-    fn data_since(
-        &self,
-        _time: chrono::DateTime<chrono::Utc>,
-    ) -> Box<dyn Iterator<Item = vatsim_stats::datafeed::Datafeed>> {
-        Box::new(iter::empty())
+async fn downloader(storage: Arc<RwLock<FlightStorage>>) {
+    let mut interval = time::interval(Duration::from_secs(30));
+    loop {
+        interval.tick().await;
+        match datafeed::download().await {
+            Ok(datafeed) => {
+                let mut guard = storage.write().await;
+                info!(
+                    "downloaded datafeed with time: {}",
+                    datafeed.general.update_timestamp
+                );
+                if datafeed.general.update_timestamp > guard.last_ts() {
+                    guard.append(datafeed);
+                }
+            }
+            Err(err) => {
+                warn!("error downloading datafeed: {}", err)
+            }
+        }
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,46 +1,13 @@
-use crate::{
-    datafeed::{self, Datafeed},
-    storage::{Flight, FlightStorage},
-};
+use crate::storage::{Flight, FlightStorage};
 use chrono::prelude::*;
-use log::{info, warn};
+use log::info;
 use serde::{Deserialize, Serialize};
-use std::time::Duration;
-use std::{convert::Infallible, fs, sync::Arc};
+use std::{convert::Infallible, sync::Arc};
 use tokio::sync::RwLock;
 use warp::{reply::Json, Filter};
 
-async fn downloader(storage: Arc<RwLock<FlightStorage>>) {
-    loop {
-        match datafeed::download().await {
-            Ok(datafeed) => {
-                let mut guard = storage.write().await;
-                info!(
-                    "downloaded datafeed with time: {}",
-                    datafeed.general.update_timestamp
-                );
-                if datafeed.general.update_timestamp > guard.last_ts() {
-                    guard.append(datafeed);
-                }
-            }
-            Err(err) => {
-                warn!("error downloading datafeed: {}", err)
-            }
-        }
-        tokio::time::delay_for(Duration::from_secs(30)).await;
-    }
-}
-
-pub async fn run(data_provider: impl DataProvider) -> anyhow::Result<()> {
+pub async fn run(flight_storage: Arc<RwLock<FlightStorage>>) -> anyhow::Result<()> {
     info!("starting up...");
-    let mut flight_storage: FlightStorage =
-        ron::from_str(&fs::read_to_string("storage/flights.ron")?)?;
-    for datafeed in data_provider.data_since(flight_storage.last_ts()) {
-        flight_storage.append(datafeed);
-    }
-    let flight_storage = Arc::new(RwLock::new(flight_storage));
-    info!("flight data loaded");
-    tokio::spawn(downloader(flight_storage.clone()));
 
     let with_flights = warp::any().map(move || flight_storage.clone());
     let flights_airport = warp::path!("flights" / "airport")
@@ -62,10 +29,6 @@ struct AirportQuery {
     icao: String,
     since: DateTime<Utc>,
     until: DateTime<Utc>,
-}
-
-pub trait DataProvider {
-    fn data_since(&self, time: DateTime<Utc>) -> Box<dyn Iterator<Item = Datafeed>>;
 }
 
 async fn flights_airport(
